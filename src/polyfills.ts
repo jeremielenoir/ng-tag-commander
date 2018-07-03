c1f0f2f4a27997f656a9fd9a89e8bd0990241022
Fabien Walesch <fabien.walesch@gmail.com>
2018-07-03 18:03:54 +0200

    commit end of day 03/07/2018

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -78,3 +78,5 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+import 'intl';
+import 'intl/locale-data/jsonp/en';
